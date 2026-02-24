94e53d4dd7e4bf804f06b03838faa668e7c0690f
Gagan sen <153493491+gagan0097sen@users.noreply.github.com>
2026-02-25 03:15:26 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,4 @@
-// 'use client';
 
-// import { AuthProvider } from './context/AuthContext';
-// import Header from './components/Header';
-// import './layout.css';
-
-// export default function RootLayout({ children }) {
-//   return (
-//     <html lang="en">
-//       <body>
-//         <AuthProvider>
-//           <Header />
-//           <main>{children}</main>
-//         </AuthProvider>
-//       </body>
-//     </html>
-//   );
-// }
 
 
 'use client';
@@ -36,4 +19,5 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
+
 
